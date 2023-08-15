ed37db757a8f63cf1cb76620a950098c4707b4d6
ndukwesamuel <ndukwesamuel23@yahoo.com>
2023-08-15 19:49:56 +0100

    new

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./home.css";
 import TodoList from "../components/TodoList";
@@ -6,6 +6,21 @@ import { Todos } from "../uitils";
 
 const Home = ({}) => {
   const [todos, setTodos] = useState(Todos);
+  const [count, setCount] = useState(0);
+
+  console.log("todos");
+
+  useEffect(() => {
+    console.log("Home");
+  }, []);
+
+  const handleCount = () => {
+    setCount((preCount) => preCount + 1);
+  };
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, [count]);
 
   const markComplete = (id) => {
     console.log(id);
@@ -24,6 +39,8 @@ const Home = ({}) => {
   return (
     <div>
       <h1>Todo</h1>
+
+      <button onClick={handleCount}>CLick {count} times </button>
       <div className="todo-list">
         <TodoList
           items={todos}
